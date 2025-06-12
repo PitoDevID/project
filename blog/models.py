e811ea80049961ee3df87d570ffa5b718c40698d
PitoDev ArmStrong <74353123+PitoDevID@users.noreply.github.com>
2025-06-12 11:38:02 +0700

    Add files via upload

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,15 @@ class Barang(models.Model):
     stok_barang = models.IntegerField()
     harga_barang = models.BigIntegerField()
     link_gambar = models.TextField(max_length=150, blank=True)
+    tgl_input = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.nama_barang
+    
+class Jenis(models.Model):
+    nama = models.CharField(max_length=78)
+    deskripsi = models.TextField()
+
+    def __str__(self):
+        return self.nama
